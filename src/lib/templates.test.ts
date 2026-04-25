95076e32928a6ecf27865dcd7a68b18c53cc8721
panbokui <1575232960@qq.com>
2026-04-25 05:50:42 -0400

    chore: remove polaroid from template library

--- src/lib/templates.test.ts
+++ src/lib/templates.test.ts
@@ -6,7 +6,6 @@ describe("template library", () => {
   test("hides polaroid until it is implemented", () => {
     expect(TEMPLATE_LIBRARY.map((template) => template.kind)).toEqual([
       "classic-bottom",
-      "polaroid",
       "minimal-corner",
     ]);
   });
